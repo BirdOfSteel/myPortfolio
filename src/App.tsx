87afcedd3b4b6308e24804d9323ff462b9b749b9
BirdOfSteel <AmirAziz2001@Gmail.com>
2025-02-01 20:45:24 +0000

    Added buttons and animations
    - Project divs use animations from React Motion now.
    
    - Added buttons to each div project. Still non-functional.

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,215 @@
 import { useState, useEffect } from 'react'
 import './App.css';
-import { motion } from 'motion/react'
+import { motion, MotionConfig, useScroll, useTransform } from 'motion/react'
 import Hero from './components/Hero'
+import { easeIn } from 'motion';
 
 function App() {
+  const { scrollYProgress } = useScroll();
+
+  const background = useTransform(
+    scrollYProgress,
+    [0, 1],
+    ['#5a5d7a', '#747fe0']
+  )
+
   return (
     <>
+      <motion.div 
+        style={{
+          scaleX: scrollYProgress,
+          originX: 0,
+          background: background,
+          width: '100%',
+          height: '10px',
+          position: 'fixed',
+          top: 0,
+          zIndex: 1
+        }}
+      />
+      
       <Hero />
       <div id='projectsDiv'>
         <div id="projectsHeadingDiv">
           <h2>Projects</h2>
           <div id="projectsHeadingDivUnderline"/>
         
-          <div className='projectDiv'>
-            <img 
-              src={'/weatherAppScreenshot.PNG'}
-              className='projectImage'  
-            />
-            <div className='projectImageOverlay'/>
-            <p className='projectDivText'>
-              Weather app that utilises data from Open-Meteo and OpenWeather APIs.
-              <br/>
-              <br/>
-              Weather data is presented as hourly and daily, with a menu in the top left for unit conversion.
-              The background changes colour depending on the time of day.             
-              <br/>
-              <br/>
-              Written in React and TypeScript.
-            </p>
-          </div>
-          <div className='projectDiv'>
-            <img 
-              src={'/calculatorAppScreenshot.png'}
-              className='projectImage'  
-            />
-            <div className='projectImageOverlay'/>
-            <p className='projectDivText'>
-              Works by collecting the inputs as a string, then allowing JavaScript to carry out the calculation.
-              The app includes checks to prevent symbols like roots from being used one after the other.
-              At the top is a style menu that can be hidden by clicking the attached button.      
-              <br/>
-              <br/>
-              Written in React.
-            </p>
-          </div>
-          <div className='projectDiv'>
-            <img 
-              src={'/triviaAppScreenshot.PNG'}
-              className='projectImage'  
-            />
-            <div className='projectImageOverlay'/>
-            <p className='projectDivText'>
-              One of my first projects! A simple trivia app that makes use of the the OpenTriviaDB API.
-              Works as you expect it would, but the start menu also lists a few dropdown menus to customise
-              gameplay options.
 
-              Written in JavaScript.
-            </p>
-          </div>
+          <MotionConfig
+            transition={{
+              duration: 1
+            }}
+          >
+
+            <motion.div className='projectDiv'
+              initial={{
+                y: '-10px',
+                opacity: 0
+              }}
+              whileInView={{
+                y: '0px',
+                opacity: 1
+              }}
+            >
+              <img 
+                src={'/weatherAppScreenshot.PNG'}
+                className='projectImage'  
+                />
+              <div className='projectImageOverlay'/>
+              <div className='projectDescriptionDiv'>
+                <motion.h1
+                  initial={{
+                    y: '-5px',
+                    opacity: 0
+                  }}
+                  whileInView={{
+                    y: '0px',
+                    opacity: 1
+                  }}
+                  transition={{
+                    ease: easeIn,
+                    duration: 0.5
+                  }}
+                >
+                  Weather app
+                </motion.h1>
+                <motion.p className='projectDivText'
+                  initial={{
+                    y: '20px',
+                    opacity: 0
+                  }}
+                  whileInView={{
+                    y: '0px',
+                    opacity: 1
+                  }}
+                  transition={{
+                    duration: 0.5,
+                    delay: 0.5
+                  }}
+                >
+                  Weather app that utilises data from Open-Meteo and OpenWeather APIs.
+                  Weather data is presented as hourly and daily, with a menu in the top left for unit conversion.
+                  Background changes colour depending on the time of day.             
+                  <br/>
+                  <br/>
+                  Written in React and TypeScript.
+                </motion.p>
+
+                <div className='projectDivButtonsDiv'> 
+                  <button className='projectLiveDemoButton'>
+                    Live Demo
+                  </button>
+                  <button className='projectSourceCodeButton'>
+                    Source Code
+                  </button>
+                </div>
+              </div>
+            </motion.div>
+            <motion.div 
+              className='projectDiv'
+              initial={{
+                y: '-10px',
+                opacity: 0
+              }}
+              whileInView={{
+                y: '0px',
+                opacity: 1
+              }}  
+            >
+              <img 
+                src={'/calculatorAppScreenshot.png'}
+                className='projectImage'  
+                />
+              <div className='projectImageOverlay'/>
+              <div className='projectDescriptionDiv'>
+                <h1>Calculator</h1>
+                <motion.p 
+                  className='projectDivText'
+                  initial={{
+                    y: '20px',
+                    opacity: 0
+                  }}
+                  whileInView={{
+                    y: '0px',
+                    opacity: 1
+                  }}
+                  transition={{
+                    duration: 0.5,
+                    delay: 0.5
+                  }}
+                >
+                  Performs mostly simple calculations, but allows for roots and powers.
+                  Uses 'if' statements to prevent certain symbols from being used consecutively.
+                  <br/>
+                  <br/>
+                  Dropdown menu at the top for a selection of styles.      
+                  <br/>
+                  <br/>
+                  Written in React.
+                </motion.p>
+                
+                <div className='projectDivButtonsDiv'> 
+                  <button className='projectLiveDemoButton'>
+                    Live Demo
+                  </button>
+                  <button className='projectSourceCodeButton'>
+                    Source Code
+                  </button>
+                </div>
+              </div>
+            </motion.div>
+            <motion.div className='projectDiv'
+              initial={{
+                y: '-10px',
+                opacity: 0
+              }}
+              whileInView={{
+                y: '0px',
+                opacity: 1
+              }}
+            >
+              <img 
+                src={'/triviaAppScreenshot.PNG'}
+                className='projectImage'  
+                />
+              <div className='projectImageOverlay'/>
+
+              <div className='projectDescriptionDiv'>
+                <h1>Trivia</h1>
+                <motion.p className='projectDivText'
+                  initial={{
+                    y: '20px',
+                    opacity: 0
+                  }}
+                  whileInView={{
+                    y: '0px',
+                    opacity: 1
+                  }}
+                  transition={{
+                    duration: 0.5,
+                    delay: 0.5
+                  }}
+                >
+                  One of my first projects! A simple trivia app that makes use of the the OpenTriviaDB API.
+                  Works as you expect it would, but the start menu also lists a few dropdown menus to customise
+                  gameplay options.
+                  <br/>
+                  <br/>
+                  Written in React.
+                </motion.p>
+
+                <div className='projectDivButtonsDiv'> 
+                  <button className='projectLiveDemoButton'>
+                    Live Demo
+                  </button>
+                  <button className='projectSourceCodeButton'>
+                    Source Code
+                  </button>
+                </div>
+              </div>
+            </motion.div>
+
+          </MotionConfig>
         
         </div>
       </div>
