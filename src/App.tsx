9f7c0b0c57458ebb33b94d549e1eccdee01f7d82
BirdOfSteel <AmirAziz2001@Gmail.com>
2025-02-01 19:51:30 +0000

    Created the layout for project divs section
    As title says

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import './App.css';
 import { motion } from 'motion/react'
 import Hero from './components/Hero'
 
-
 function App() {
   return (
     <>
@@ -12,8 +11,55 @@ function App() {
         <div id="projectsHeadingDiv">
           <h2>Projects</h2>
           <div id="projectsHeadingDivUnderline"/>
-        </div>
+        
+          <div className='projectDiv'>
+            <img 
+              src={'/weatherAppScreenshot.PNG'}
+              className='projectImage'  
+            />
+            <div className='projectImageOverlay'/>
+            <p className='projectDivText'>
+              Weather app that utilises data from Open-Meteo and OpenWeather APIs.
+              <br/>
+              <br/>
+              Weather data is presented as hourly and daily, with a menu in the top left for unit conversion.
+              The background changes colour depending on the time of day.             
+              <br/>
+              <br/>
+              Written in React and TypeScript.
+            </p>
+          </div>
+          <div className='projectDiv'>
+            <img 
+              src={'/calculatorAppScreenshot.png'}
+              className='projectImage'  
+            />
+            <div className='projectImageOverlay'/>
+            <p className='projectDivText'>
+              Works by collecting the inputs as a string, then allowing JavaScript to carry out the calculation.
+              The app includes checks to prevent symbols like roots from being used one after the other.
+              At the top is a style menu that can be hidden by clicking the attached button.      
+              <br/>
+              <br/>
+              Written in React.
+            </p>
+          </div>
+          <div className='projectDiv'>
+            <img 
+              src={'/triviaAppScreenshot.PNG'}
+              className='projectImage'  
+            />
+            <div className='projectImageOverlay'/>
+            <p className='projectDivText'>
+              One of my first projects! A simple trivia app that makes use of the the OpenTriviaDB API.
+              Works as you expect it would, but the start menu also lists a few dropdown menus to customise
+              gameplay options.
 
+              Written in JavaScript.
+            </p>
+          </div>
+        
+        </div>
       </div>
     </>
   )
